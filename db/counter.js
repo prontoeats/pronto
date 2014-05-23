db67e6953f4bfdf60d2173edd43c79127a869798
Jeffrey Gho <jeffreygho@gmail.com>
2014-05-22 17:15:03 -0700

    Mongoose does not like the use of modelName, so switched to tableName

--- db/counter.js
+++ db/counter.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var blue = require('bluebird');
 
 var counterSchema = mongoose.Schema({
-  modelName:    {type: String},
+  tableName:    {type: String},
   counter:      {type: Number, default: 0}
 });
 
@@ -10,10 +10,10 @@ var Counter = mongoose.model('Counter', counterSchema);
 
 Counter.promFindOneAndUpdate = blue.promisify(Counter.findOneAndUpdate);
 
-Counter.getCounter = function (modelName) {
+Counter.getCounter = function (tableName) {
   return Counter.promFindOneAndUpdate(
     // conditions
-    {modelName: modelName},
+    {tableName: tableName},
     // update: increases counter field by 1)
     {$inc: {counter: 1}},
     // options: returns modified document, creates if DNE, return counter
@@ -26,12 +26,12 @@ Counter.getCounter('vendors');
 exports.Counter = Counter;
 
 //used to create a new counter if the counter collection is deleted
-// // new Counter({modelName: 'requests'}).save();
+// // new Counter({tableName: 'requests'}).save();
 
 //returns
 // Counter.getRequestsCounter = function(){
 //   return Counter.promFindOneAndUpdate(
-//     {modelName: 'requests'},
+//     {tableName: 'requests'},
 //     { $inc: {counter:1} },
 //     {new: true,
 //       select: 'counter'}
