e469b256b99bf12b4a1604a00ec25d256be28ad2
Jeffrey Gho <jeffreygho@gmail.com>
2014-05-30 15:57:38 -0700

    Relative time of request

--- client/platforms/ios/www/js/app.js
+++ client/platforms/ios/www/js/app.js
@@ -6,7 +6,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'angularMoment'])
 
 .run(function($ionicPlatform, $state, localStorageService) {
 // .run(function($ionicPlatform) {
