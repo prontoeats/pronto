3b46482fc530d2132cdae115aa7196de0dfa21f4
JoeDou <joe.dou@gmail.com>
2014-06-02 13:10:35 -0700

    fix app.js client side bug

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -28,10 +28,10 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     // localStorageService.set('user', 'false');
     // $state.transitionTo('rest.requests');
 
-    localStorageService.set('token', 'ya29.KQCA_F3XfDcaURoAAADa9zaMesu2janbVejDoOEYJ8PXwQ2TxhosBW4313iuFA');
-    localStorageService.set('userId', '538bea45c4dfa1902bb66e70');
-    localStorageService.set('user', 'true');
-    $state.transitionTo('user.new');
+    // localStorageService.set('token', 'ya29.KQCA_F3XfDcaURoAAADa9zaMesu2janbVejDoOEYJ8PXwQ2TxhosBW4313iuFA');
+    // localStorageService.set('userId', '538bea45c4dfa1902bb66e70');
+    // localStorageService.set('user', 'true');
+    // $state.transitionTo('user.new');
 
     var token = localStorageService.get('token');
 
