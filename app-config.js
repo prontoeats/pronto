a7d07e2592c8b3cd3a844e5ea5a028e139d5531d
Jeffrey Gho <jeffreygho@gmail.com>
2014-06-03 13:42:04 -0700

    Only allow GET and POST requests

--- app-config.js
+++ app-config.js
@@ -8,7 +8,7 @@ var app = express();
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET,POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 }
