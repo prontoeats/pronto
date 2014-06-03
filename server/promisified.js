aaec19511389c067eb45969f7ad457f1ca82c556
Jeffrey Gho <jeffreygho@gmail.com>
2014-06-03 14:22:13 -0700

    Remove bcrypt from promisified.js as no longer used

--- server/promisified.js
+++ server/promisified.js
@@ -1,9 +1,5 @@
 
 var blue = require('bluebird');
-var bcrypt = require('bcrypt-nodejs');
 var request = require('request');
 
-//Promisifying functions
-exports.bcryptHash = blue.promisify(bcrypt.hash);
-exports.bcryptCompare = blue.promisify(bcrypt.compare);
 exports.request = blue.promisify(request);
