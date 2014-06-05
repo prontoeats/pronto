092c69d857d57318842c3caf599f02308e577591
Jimmy Tsao <jimmy.tsao@gmail.com>
2014-06-04 19:20:31 -0700

    Changed build name to Pronto

--- client/platforms/android/assets/www/cordova_plugins.js
+++ client/platforms/android/assets/www/cordova_plugins.js
@@ -1,24 +1,17 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/com.ionic.keyboard/www/keyboard.js",
-        "id": "com.ionic.keyboard.keyboard",
-        "clobbers": [
-            "cordova.plugins.Keyboard"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.device/www/device.js",
-        "id": "org.apache.cordova.device.device",
-        "clobbers": [
-            "device"
-        ]
-    },
-    {
         "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
         "id": "org.apache.cordova.inappbrowser.inappbrowser",
         "clobbers": [
             "window.open"
+        ]
+    },
+    {
+        "file": "plugins/com.ionic.keyboard/www/keyboard.js",
+        "id": "com.ionic.keyboard.keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
         ]
     },
     {
@@ -27,17 +20,24 @@ module.exports = [
         "clobbers": [
             "PushNotification"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.ionic.keyboard": "0.0.1",
-    "org.apache.cordova.console": "0.2.9-dev",
-    "org.apache.cordova.device": "0.2.10-dev",
-    "org.apache.cordova.geolocation": "0.3.8-dev",
     "org.apache.cordova.inappbrowser": "0.4.1-dev",
-    "com.phonegap.plugins.PushPlugin": "2.2.0"
+    "com.ionic.keyboard": "0.0.1",
+    "com.phonegap.plugins.PushPlugin": "2.2.0",
+    "org.apache.cordova.console": "0.2.8",
+    "org.apache.cordova.device": "0.2.10-dev",
+    "org.apache.cordova.geolocation": "0.3.8-dev"
 }
 // BOTTOM OF METADATA
 });
